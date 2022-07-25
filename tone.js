72d41af504a631e99950fc883384d0e5cf32f8d1
Sergey Vershkov <sergey.vershkov@gmail.com>
2022-07-25 14:49:42 +0000

    Adds comments

--- tone.js
+++ tone.js
@@ -1,25 +1,29 @@
 'use strict';
 
+//Global consts
 const FREQ = 8000;       
+const BITS_PER_VOICE_SAMPLE = 16;
+
+//Local class consts
 const PI = Math.PI;
 const TWO_POW_32 = Math.pow(2, 32);
 const TWO_POW_31 = Math.pow(2, 31);
-const BITS_PER_SAMPLE = 16;
-const TWO_POW_BITS_PER_SAMPLE = Math.pow(2, BITS_PER_SAMPLE);
+const TWO_POW_BITS_PER_SAMPLE = Math.pow(2, BITS_PER_VOICE_SAMPLE);
 const PI_DIVIDE_BY_TWO_POW_31 = PI/TWO_POW_31;
 const TWO_POW_32_DIVIDE_BY_FREQ = TWO_POW_32/FREQ;
 
+//Define the class for generate tone signals
 class ToneSignal {
-    constructor(freq, i_nom_level) {
+    constructor(freq, i_nom_level) {                    //Constructor
         this.level_old = this.nom_level = i_nom_level; 
         this.freq_old = freq;
-        this.inc_arg = Math.round((freq*TWO_POW_32_DIVIDE_BY_FREQ));  
+        this.inc_arg = Math.round(freq*TWO_POW_32_DIVIDE_BY_FREQ);  
         this.arg = 0;
         this.out_float = 0;
         this.out_int = 0;
     }
 
-    generator_float() {    
+    generator_float() {                                 //Generate float tone signal
         let out_gen = 0;
         out_gen = this.nom_level * Math.sin(PI_DIVIDE_BY_TWO_POW_31 * this.arg);
 	    
@@ -32,18 +36,18 @@ class ToneSignal {
 	    return this.out_float;
     }
 
-    generator_int() {    
+    generator_int() {                                   //Generate int tone signal
         this.out_int = Math.round(this.generator_float() * TWO_POW_BITS_PER_SAMPLE);   
 	    return this.out_int;
     }
 
-    reset() {
+    reset() {                                           //Reset the generator tone signal
         this.arg = 0;
         this.out_float = 0;
         this.out_int = 0;
     }
 
-    change_freq(freq) {
+    change_freq(freq) {                                 //Change the frequency of tone signal
         if (freq != this.freq_old) {
             this.freq_old = this.freq;
             this.freq = freq;
@@ -52,7 +56,7 @@ class ToneSignal {
         }
     }
     
-    change_level(level) {
+    change_level(level) {                               //Change the level of tone signal
         if (level != this.nom_level) {
             this.nom_level = level;
             this.reset();
